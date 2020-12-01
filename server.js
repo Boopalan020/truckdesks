fd932ef86202d7d7b69a01703ff210f25638cd59
M Boopalan <boopalan020@gmail.com>
2020-12-01 11:42:15 +0530

    to deploy

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const vehicle = require('./routes/vehicles')
 const memo = require('./routes/memo')
 const dashboard = require('./routes/dashboard')
 
+const path = require('path')
+
 const keys = require('./config/key')
 const port = process.env.PORT || 3001
 // Mongoose connection
@@ -20,12 +22,16 @@ mongoose.connect(keys.mongodb.dbURI, {useNewUrlParser: true, useUnifiedTopology:
 })
 app.use(morgan("dev"))
 app.use(cors({
-    origin : "http://localhost:3000"
+    origin : "https://truckdesks.herokuapp.com/"
 }))
 
 app.use('/dashboard', dashboard)
 app.use('/drivers', driver)
 app.use('/vehicle', vehicle)
 app.use('/memo', memo)
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build', 'index.html'))
+})
 
 app.listen(port, () => console.log("Server is running at PORT ===" + port))
